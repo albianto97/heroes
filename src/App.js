1816b75bd4d508bdbf9a1a5d53ab8f41a1835008
albianto97 <albyantox@gmail.com>
2021-07-21 11:17:51 +0200

    Aggiunta di hero

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const heroes = [
+  {
+    id: 1,
+    name: 'Hulk',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    name: 'Spiderman',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    name: 'Superman',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
+const  App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>Heroes</h1>
+        <ul>
+          {heroes.map(h => <li key= {h.id}> {h.name}</li>)}
+        </ul>
+      </div>
   );
 }
 
