d389bf839b4ac5115546ef1184e759d6d834bdd3
albianto97 <albyantox@gmail.com>
2021-07-21 15:37:49 +0200

    Fine copia e incolla

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, {useState,useEffect} from 'react'
+import './App.css'
 import Hero from './components/Hero.js'
+import axios from 'axios'
 const App = () => {
-    const heroes = [
-        {
-            id: 1,
-            name: 'Hulk',
-            date: '2019-05-30T17:30:31.098Z',
-            important: true
-        },
-        {
-            id: 2,
-            name: 'Spiderman',
-            date: '2019-05-30T18:39:34.091Z',
-            important: false
-        },
-        {
-            id: 4,
-            name: 'Superman',
-            date: '2019-05-30T19:20:14.298Z',
-            important: true
-        }
-    ]
-    const [eroi, setEroi] = useState(heroes)
+    const [heroes, setHeroes] = useState([])
     const [newHero, setNewHero] = useState('')
     const [showAll, setShowAll] = useState(true)
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/heroes')
+            .then(response => {
+                console.log('promise fulfilled')
+                setHeroes(response.data)
+            })
+    }, [])
+    console.log('render', heroes.length, 'notes')
 
     const heroesToShow = showAll
         ? heroes
         : heroes.filter(hero => hero.important === true)
-    const rows = () => eroi.map(hero =>
+    const rows = () => heroes.map(hero =>
         <Hero
             key={hero.id}
             hero={hero}
@@ -44,7 +35,7 @@ const App = () => {
             important: Math.random() > 0.5,
             id: heroes.length + 1,
         }
-        setEroi(heroes.concat(heroObject))
+        setHeroes(heroes.concat(heroObject))
         setNewHero('')
     }
     const handleHeroChange = event => {
@@ -64,6 +55,7 @@ const App = () => {
             <form onSubmit={addHero}>
                 <input
                     type='text'
+                    placeholder='Add Hero...'
                     value={newHero}
                     onChange={handleHeroChange}
                 />
@@ -73,4 +65,4 @@ const App = () => {
     );
 }
 
-export default App;
+export default App
